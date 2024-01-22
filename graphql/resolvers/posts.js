807ec105bd4545f6080c8aa24c2c88c3131a4a0a
Chaitanya Chaurasiya <1011chaitanya@gmail.com>
2024-01-22 21:34:23 +0530

    Added create post and delete post backend functionality

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,11 +1,59 @@
+const { AuthenticationError } = require("apollo-server");
 const Post = require("../../models/Post");
+const checkAuth = require("../../utils/check-auth");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.deleteOne();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Delete operation is not allowed");
+        }
       } catch (e) {
         throw new Error(e);
       }
