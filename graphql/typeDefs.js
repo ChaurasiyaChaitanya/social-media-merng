eab8484b8e92e54ead87fa1f2582a5ab8792b23f
Chaitanya Chaurasiya <1011chaitanya@gmail.com>
2024-01-24 21:34:33 +0530

    Like and Comment on Post functionality

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,21 @@ module.exports = gql`
     body: String!
     username: String!
     createdAt: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
+  }
+  type Comment {
+    id: ID!
+    username: String!
+    body: String!
+    createdAt: String!
+  }
+  type Like {
+    id: ID!
+    username: String!
+    createdAt: String!
   }
   type User {
     id: ID!
@@ -29,5 +44,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
